bd542a2a64bfedbdf225a1c2e294dc26f940eee2
akaRicane <ricane@lbss.art>
2024-02-23 00:30:30 +0100

    dark mode enabled

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from '@componentsui/button';
+"use client";
+
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
-      <Button>Click me!</Button>
+      <Button
+        className="mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
+        onClick={() => {
+          setTheme(theme === "light" ? "dark" : "light");
+        }}
+      >
+        Change Theme
+      </Button>
     </>
   );
 }
