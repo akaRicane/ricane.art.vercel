3b76488c01d9c165d32effaaa42595c8e8275b47
akaRicane <ricane@lbss.art>
2024-02-23 01:40:00 +0100

    dark mode icons

--- components/theme/ThemeToggle.tsx
+++ components/theme/ThemeToggle.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import { Moon, SunMedium } from "lucide-react";
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
@@ -15,7 +16,8 @@ export const ThemeToggle = () => {
         setTheme(theme === "light" ? "dark" : "light");
       }}
     >
-      Change Theme
+      <SunMedium className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   );
 };
